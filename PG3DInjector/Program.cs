fe108863b8d8138da1df1b8492192365c61cfe7d
AkiraDev <akira.devjs@gmail.com>
2024-04-16 20:37:14 -0700

    frick minhook

--- PG3DInjector/Program.cs
+++ PG3DInjector/Program.cs
@@ -3,6 +3,5 @@
 using PG3DInjector;
 
 Console.WriteLine("Injecting...");
-Inject.Load("Pixel Gun 3D", Path.GetFullPath("./minhook.x64.dll"));
 Inject.Load("Pixel Gun 3D", Path.GetFullPath("./PixelGunCheat.dll"));
 Console.WriteLine("Injected");
